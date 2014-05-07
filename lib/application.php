0562189de8185ee91980de09a12e5919e5ae38aa
Nick DeNardis <nick.denardis@gmail.com>
2014-05-07 19:43:33 -0400

    Starting from scratch.

---
+++ lib/application.php
@@ -0,0 +1,17 @@
+<?php namespace Simpl;
+
+/**
+ * Base PHPSimpl Class used to control Simpl at its highest level
+ *
+ * @author Nick DeNardis <nick.denardis@gmail.com>
+ * @link https://github.com/nickdenardis/phpsimpl
+ */
+class Application
+{
+
+    public function __construct()
+    {
+        
+    }
+
+}
